d05e300de4cbb427148414cc79d9b7e7fa5e493c
Saptyadeep Bhattacharjee <78376701+bsaptyadeep@users.noreply.github.com>
2023-04-14 21:10:29 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ const HomePage = () => {
         <div className='home-page'>
             <LandingScreen />
             <Event/>
-            <Socials />
+//             <Socials />
             <AboutUs />
             <Sponsors />
             <ContactUs />
