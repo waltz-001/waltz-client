760b9e6658458644ee60884d7f334ba3586157d1
ayushjalan <ayushjalan1203@gmail.com>
2023-04-14 20:24:16 +0530

    Added Event component to the website

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LandingScreen from '../components/LandingScreen/LandingScreen';
+import Event from '../components/Event/Events'
 import AboutUs from '../components/AboutUs/AboutUs';
 import Sponsors from '../components/Sponsors/Sponsors';
 import ContactUs from '../components/ContactUs/ContactUs';
@@ -10,6 +11,7 @@ const HomePage = () => {
     return (
         <div className='home-page'>
             <LandingScreen />
+            <Event/>
             <Socials />
             <AboutUs />
             <Sponsors />
