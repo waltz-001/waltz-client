a15720475b57cbf956cddf7693713709624a8021
Saptyadeep <bsaptyadeep@gmail.com>
2023-04-17 10:13:59 +0530

    update logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,79 +1,79 @@
-import React, {useState} from 'react';
-import Logo from '../../assests/images/logo.png';
+import React, { useState } from 'react';
+import Logo from '../../assests/images/WALTZ 2K23 LOGO (PNG).png';
 import './Navbar.css';
 
 const Navbar = () => {
-    const [navBar, setNavBar] = useState(false);
+  const [navBar, setNavBar] = useState(false);
 
-    const changeBackground = () => {
-        if(window.scrollY>=80)
-        setNavBar(true)
-        else
-        setNavBar(false)
-    }
+  const changeBackground = () => {
+    if (window.scrollY >= 80)
+      setNavBar(true)
+    else
+      setNavBar(false)
+  }
 
-    window.addEventListener('scroll', changeBackground)
+  window.addEventListener('scroll', changeBackground)
 
-    return (
-        <>
-        <nav className={navBar?'navbar-container active navbar-fixed-top navbar-expand-lg':'navbar-container navbar-expand-md '}>
+  return (
+    <>
+      <nav className={navBar ? 'navbar-container active navbar-fixed-top navbar-expand-lg' : 'navbar-container navbar-expand-md '}>
 
-        <a className="navbar-brand p-1" href="/"><img src={Logo} alt="" style={{width:"6rem"}} /></a>
-{/* OFFCANVAS */}
+        <a className="navbar-brand p-1" href="/"><img src={Logo} alt="" style={{ width: "3rem" }} /></a>
+        {/* OFFCANVAS */}
 
-  <label htmlFor="navbar-toggle" className="navbar-toggler togglebtn " data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-    <span className="navbar-toggler-icon"></span>
-  </label>      
+        <label htmlFor="navbar-toggle" className="navbar-toggler togglebtn " data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+          <span className="navbar-toggler-icon"></span>
+        </label>
 
 
-<div className="offcanvas offcanvas-start bg-dark" data-bs-backdrop="static" tabIndex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
-  <div className="offcanvas-header">
-    <h5 className="offcanvas-title" id="staticBackdropLabel"><a className="navbar-brand" href="/"><img className='mt-4' src={Logo} alt="" style={{width:"6rem"}} /></a></h5>
-    <button type="button" className="btn-close bg-warning" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div className="offcanvas-body">
-    <div>
-      
-    </div>
-    <div className="navbar-option">
-            
-                <ul className='text-center'>
-                    <li className='my-5 nav-item'><a href='/'>HOME</a></li>
-                    <li className='my-5 nav-item'><a href='#about-us'>ABOUT US</a></li>
-                    <li className='my-5 nav-item'><a href='#sponsors'>SPONSORS</a></li>
-                    <li className='my-5 nav-item'><a href='#contact-us'>CONTACT US</a></li>
-                </ul>
+        <div className="offcanvas offcanvas-start bg-dark" data-bs-backdrop="static" tabIndex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="staticBackdropLabel"><a className="navbar-brand" href="/"><img className='mt-4' src={Logo} alt="" style={{ width: "3rem" }} /></a></h5>
+            <button type="button" className="btn-close bg-warning" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <div>
+
             </div>
-  </div>
-</div>
+            <div className="navbar-option">
+
+              <ul className='text-center'>
+                <li className='my-5 nav-item'><a href='/'>HOME</a></li>
+                <li className='my-5 nav-item'><a href='#about-us'>ABOUT US</a></li>
+                <li className='my-5 nav-item'><a href='#sponsors'>SPONSORS</a></li>
+                <li className='my-5 nav-item'><a href='#contact-us'>CONTACT US</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
 
-  
+
         <div className="navbar-option-list">
-            
-                <ul>
-                    <li className='mt-2'><a href='/'>HOME</a></li>
-                    <li className='mt-2'><a href='#about-us'>ABOUT US</a></li>
-                    <li className='mt-2'><a href='#sponsors'>SPONSORS</a></li>
-                    <li className='mt-2'><a href='#contact-us'>CONTACT US</a></li>
-                </ul>
-            </div>
-            <div className='navbar-option-icons mt-2'>
-                {/* <img src={SearchIcon} height={19} alt='search-icon' />
+
+          <ul>
+            <li className='mt-2'><a href='/'>HOME</a></li>
+            <li className='mt-2'><a href='#about-us'>ABOUT US</a></li>
+            <li className='mt-2'><a href='#sponsors'>SPONSORS</a></li>
+            <li className='mt-2'><a href='#contact-us'>CONTACT US</a></li>
+          </ul>
+        </div>
+        <div className='navbar-option-icons mt-2'>
+          {/* <img src={SearchIcon} height={19} alt='search-icon' />
                 <img src={UserIcon} height={19} alt='user-icon' /> */}
-            </div>
-            
-        </nav>
+        </div>
+
+      </nav>
 
 
 
 
-  
 
-     
 
-        </>
-    )
+
+
+    </>
+  )
 }
 
 export default Navbar
