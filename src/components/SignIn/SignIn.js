d87c3f9c8c4c1a646a69691b4103ca8c95775791
SANJIB KUMAR DAS <sanjibdas.23012002@gmail.com>
2023-04-19 21:18:35 +0530

    responsive signin page
    responsive signin, resolved console errors

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -10,48 +10,48 @@ function SignIn() {
     <>
     <div className='signin-container containe'>
       <h1 className='signin-head-txt'>Let's Get Classy</h1>
-      <div className="card-container borde" style={{width:"70%", height:"80%"}}>
-      <div class="card borde mb-3 shadow rounded" style={{width: "90%", height:"90%"}}>
-  <div class="row g-0">
-    <div class="col-md-6 borde part1 shadow rounded">
-      {/* <img src={Box} class="img-fluid rounded-start boximg" alt="..."/> */}
+      <div className="card-container borde" style={{height:"80%"}}>
+      <div className="card borde mb-3 shadow rounded" style={{width: "90%", height:"90%"}}>
+  <div className="row g-0">
+    <div className="col-md-6 borde part1 shadow rounded">
+      {/* <img src={Box} className="img-fluid rounded-start boximg" alt="..."/> */}
     </div>
-    <div class="col-md-6 container borde part2 shadow rounded">
-      <div class="card-body">
-        <h5 class="card-title text-center borde">Sign In</h5>
-        <p class="card-text">
-        <div class="input-group mb-3 email-box">
-  <span class="input-group-text px-4"><i class="fa-solid fa-envelope fa-beat fa-xl" style={{color:"#DFB6FF"}}></i></span>
-  <div class="form-floating">
-    <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm"/>
-    <label for="colFormLabelSm" className='text-secondary'>Email</label>
-  </div>
-</div>
-<div class="input-group mb-1 pd-box">
-<span class="input-group-text px-4"><i class="fa-solid fa-unlock-keyhole fa-bounce fa-xl" style={{color:"#DFB6FF"}}></i></span>
-<div class="form-floating">
+    <div className="col-md-6 container borde part2 shadow rounded">
+      <div className="card-body">
+        <h5 className="card-title text-center borde">Sign In</h5>
+        <p className="card-text">
+        <span className="input-group mb-3 email-box">
+  <span className="input-group-text px-4"><i className="fa-solid fa-envelope fa-beat fa-xl" style={{color:"#DFB6FF"}}></i></span>
+  <span className="form-floating">
+    <input type="email" className="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm"/>
+    <label htmlFor="colFormLabelSm" className='text-secondary'>Email</label>
+  </span>
+</span>
+<span className="input-group mb-1 pd-box">
+<span className="input-group-text px-4"><i className="fa-solid fa-unlock-keyhole fa-bounce fa-xl" style={{color:"#DFB6FF"}}></i></span>
+<span className="form-floating">
 
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-  <label for="floatingPassword" className='text-secondary'>Password</label>
-</div>
-</div>
+  <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+  <label htmlFor="floatingPassword" className='text-secondary'>Password</label>
+</span>
+</span>
 
 {/* Remember me section */}
 
-<div class="d-grid gap-2">
-<span class="mb-4 text-secondary d-flex justify-content-between align-items-center justify-content-center" type="button" style={{height:"3rem", width:"100%"}}>
+<span className="d-grid gap-2">
+<span className="mb-4 text-secondary d-flex justify-content-between align-items-center justify-content-center" type="button" style={{height:"3rem", width:"100%"}}>
   <span className='rem '><input type="checkbox" /> Remember me</span>
   <span className='forg'><a href="/">Forgot password?</a></span>
 </span>
-</div>
+</span>
 
 {/* Button section */}
-<div class="d-grid gap-2">
-  <button class="btn text-white rounded-pill d-flex mx-auto align-items-center justify-content-center" type="button" style={{height:"3rem", width:"50%", backgroundColor:"#A023BF"}}>SIGN IN</button>
-</div>
-<div class="d-grid gap-2">
-  <span class="borde mt-3 text-secondary d-flex mx-auto align-items-center justify-content-center" type="button" style={{height:"3rem", width:"100%"}}> ————— or continue with —————</span>
-  <div className='d-flex mx-auto mt-2 gap-5'>
+<span className="d-grid gap-2">
+  <button className="btn text-white rounded-pill d-flex mx-auto align-items-center justify-content-center" type="button" style={{height:"3rem", width:"50%", backgroundColor:"#A023BF"}}>SIGN IN</button>
+</span>
+<span className="d-grid gap-2">
+  <span className="borde mt-3 text-secondary d-flex mx-auto align-items-center justify-content-center" type="button" style={{height:"3rem", width:"100%"}}> ————— or continue with —————</span>
+  <span className='d-flex mx-auto mt-2 gap-5'>
             <a
               href="/"
               target="_blank"
@@ -72,8 +72,8 @@ function SignIn() {
               {/* <i className="fab fa-google  fa-flip"></i> */}
               <img src={facebook} alt="" className='fa-flip fab' style={{width:"2.2rem"}}/>
             </a>
-          </div>
-</div>
+          </span>
+</span>
         </p>
       </div>
     </div>
