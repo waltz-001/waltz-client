70ce31f4ff3f29a78c4a13121cd462c296f7f344
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-05-09 22:14:11 +0530

    Update Buttons in landing screen

--- src/components/LandingScreen/LandingScreen.js
+++ src/components/LandingScreen/LandingScreen.js
@@ -4,6 +4,7 @@ import Logo2 from "../../assests/images/newHeading2.png";
 import Timer from "../../utils/Timer/Timer";
 import { Link } from "react-router-dom";
 import UserContext from "../../utils/UserContext";
+import { HashLink } from "react-router-hash-link";
 
 const LandingScreen = () => {
   const { user, setUser } = useContext(UserContext);
@@ -14,18 +15,23 @@ const LandingScreen = () => {
           <img src={Logo2} alt="logo" />
         </div>
         <Timer />
+    
         <div className="landing-button-container ">
+          <HashLink smooth to={"/#about"}>
           <div className="button-solid-round view-btn mt-5 ">
             <p className="mt-3">
-              <Link to={"/#about"}>VIEW DETAILS</Link>
+              VIEW DETAILS
             </p>
           </div>
+          </HashLink>
           {user.token === "" ? (
+              <Link to="/register">
             <div className="button-solid-round register-btn mt-5 ">
               <p className="mt-3">
-                <Link to="/register">REGISTER NOW</Link>
+              REGISTER NOW
               </p>
             </div>
+            </Link>
           ) : null}
         </div>
       </div>
