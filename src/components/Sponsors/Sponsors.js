dfde8181f918cb7402a92d49f39b4b3619c87d61
Pranay Pal <pny1cl2125@gmail.com>
2023-05-26 16:31:20 +0530

    Pretify

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -4,13 +4,13 @@ import "./Sponsors.css";
 import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import { HashLink } from "react-router-hash-link";
-import { Tilt } from 'react-tilt'
+import { Tilt } from "react-tilt";
 import NewLifeHospital from "./SponsorImages/NewLifeHospital.jpeg";
 import PeachNCream from "./SponsorImages/PeachNCream.jpeg";
 import Sarachi from "./SponsorImages/Sarachi.jpeg";
 import Pearl from "./SponsorImages/Pearl.png";
 import ISBM from "./SponsorImages/ISBM.png";
-import FitnessFactory from "./SponsorImages/FitnessFactory.png"
+import FitnessFactory from "./SponsorImages/FitnessFactory.png";
 function useIntersectionObserver() {
   const [inView, setInView] = useState(false);
   const ref = useRef(null);
@@ -33,17 +33,17 @@ function useIntersectionObserver() {
   return [ref, inView];
 }
 const defaultTiltOptions = {
-    reverse:        false,  // reverse the tilt direction
-    max:            35,     // max tilt rotation (degrees)
-    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
-    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
-    speed:          1000,   // Speed of the enter/exit transition
-    transition:     true,   // Set a transition on enter/exit.
-    axis:           null,   // What axis should be disabled. Can be X or Y.
-    reset:          true,    // If the tilt effect has to be reset on exit.
-    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-}
-function SponsorCard({SponsorImage, SponsorName, SponsorType}) {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
+function SponsorCard({ SponsorImage, SponsorName, SponsorType }) {
   return (
     <Tilt options={defaultTiltOptions} className="SponsorCard">
       <img src={SponsorImage} alt={SponsorName} className="SponsorImage" />
